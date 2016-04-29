288ddf0fb8f1ef59c0debddbe40edff1c1db66a4
Tapan <tapan@agilemedialab.in>
2016-04-29 13:27:51 +0530

    seperated config variables and randomized query generation

--- main.py
+++ main.py
@@ -1,72 +1,15 @@
 import pymysql.cursors
 from timer import Timer
 import queryBuilder
-
-dbConfig = {
-    'user'   : 'root',
-    'host'   : 'localhost',
-    'db'     : 'test',
-    'charset': 'latin1'
-}
-
-selectObject = "url"
-
-tableName = "wp_posts"
-
-singleQueryConfig = (
-    ("id",        "=", 15466                ),
-    ("ES",        "=", 1                    ),
-    ("US",        "=", 1                    ),
-    ("MX",        "=", 0                    ),
-    ("CO",        "=", 0                    ),
-    ("special",   "=", 0                    ),
-    ("published", "=", "2011-11-08 04:19:18"),
-    ("published", ">", "2011-11-08 04:19:18"),
-    ("published", "<", "2011-11-08 04:19:18")
-)
-collectionQueryConfig = []
-collectionQueryConfig.append((
-    ("ES",        "=", 1                    ),
-    ("US",        "=", 1                    ),
-    ("type",      "=", "video"              ),
-    ("published", ">", "2011-11-08 04:19:18"),
-    ("special",   "=", 1                    )
-))
-
-collectionQueryConfig.append((
-    ("ES",        "=", 0                    ),
-    ("US",        "=", 0                    ),
-    ("type",      "=", "slideshow"          ),
-    ("published", "<", "2011-11-08 04:19:18"),
-    ("special",   "=", 1                    )
-))
-
-collectionQueryConfig.append((
-    ("ES",        "=", 1                    ),
-    ("US",        "=", 0                    ),
-    ("type",      "=", "ecommerce"          ),
-    ("published", ">", "2013-11-08 04:19:18"),
-    ("special",   "=", 0                    )
-))
-
-collectionQueryConfig.append((
-    ("ES",        "=", 0                    ),
-    ("US",        "=", 1                    ),
-    ("type",      "=", "brand_article_video"),
-    ("published", "<", "2015-11-08 04:19:18"),
-    ("special",   "=", 1                    )
-))
-
-limit = 100
-offset = 1345
+import config
 
 def singleQueryProcess():
-    connection = pymysql.connect(**dbConfig)
+    connection = pymysql.connect(**config.dbConfig)
     cursor = connection.cursor()
     timer = Timer()
     # rowsTraversed = None
-    for clause in singleQueryConfig:
-        query = queryBuilder.single(selectObject, tableName, clause)
+    for clause in config.singleQueryConfig:
+        query = queryBuilder.single(config.selectProperty, config.tableName, clause)
         print("Executing query =====> {}".format(query))
         meanTime = 0
         for executions in range(1, 6):
@@ -82,18 +25,19 @@ def singleQueryProcess():
     connection.close()
 
 def collectionQueryProcess(whereClauses):
-    connection = pymysql.connect(**dbConfig)
+    connection = pymysql.connect(**config.dbConfig)
     cursor = connection.cursor()
     timer = Timer()
     # rowsTraversed = None
     meanTime = 0
-    query = queryBuilder.collection(selectObject, tableName, whereClauses, limit, offset)
+    query = queryBuilder.collection(config.selectProperty, config.tableName, whereClauses, config.queryRange, config.limit)
     print("Executing query =====> {}".format(query))
     for executions in range(1, 6):
         timer.restart()
         cursor.execute(query)
         meanTime += timer.get_seconds()
     print("Mean query execution time : {:.5f} seconds ".format(meanTime / 5))
+    print("")
     # cursor.execute("desc " + query)
     # rowsTraversed = cursor.fetchone()[8]
     # print("Rows Traversed : {}".format(rowsTraversed))
@@ -102,5 +46,5 @@ def collectionQueryProcess(whereClauses):
 
 singleQueryProcess()
 
-for whereClauses in collectionQueryConfig:
+for whereClauses in config.collectionQueryConfig:
     collectionQueryProcess(whereClauses)
--- queryBuilder.py
+++ queryBuilder.py
@@ -1,12 +1,12 @@
-def single(property, tableName, clause):
-    query = "SELECT {} FROM `{}` WHERE ".format(property, tableName)
+def single(selectProperty, tableName, clause):
+    query = "SELECT {} FROM `{}` WHERE ".format(selectProperty, tableName)
     if type(clause[2]) is str:
         query += " {} {} '{}'".format(clause[0], clause[1], clause[2])
     else:
         query += " {} {} {}".format(clause[0], clause[1], clause[2])
     return query
 
-def collection(property, tableName, whereClauses, limit, offset):
+def collection(selectProperty, tableName, whereClauses, queryRange, limit):
     query = []
     
     for clause in whereClauses:
@@ -14,8 +14,10 @@ def collection(property, tableName, whereClauses, limit, offset):
             query.append(" {} {} '{}'".format(clause[0], clause[1], clause[2]))
         else:
             query.append(" {} {} {}".format(clause[0], clause[1], clause[2]))
+    query.append(" id >= {}".format(queryRange))
     query = " AND".join(query)
 
-    query = "SELECT {} FROM `{}` WHERE ".format(property, tableName) + query
-    query = query + " LIMIT {} OFFSET {}".format(limit, offset)
+
+    query = "SELECT {} FROM `{}` WHERE ".format(selectProperty, tableName) + query
+    query = query + " LIMIT {}".format(limit)
     return query
